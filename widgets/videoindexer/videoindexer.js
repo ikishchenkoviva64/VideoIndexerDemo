95d9793741cbea478f9da7fb0dddf70f348d1edd
Ivan Kiishchenko <ikishchenko@dalet.local>
2017-10-31 19:05:15 +0200

    Improve video selection

--- widgets/videoindexer/videoindexer.js
+++ widgets/videoindexer/videoindexer.js
@@ -1,5 +1,6 @@
 angular.module('daletVideoIndexer', [])
     .controller('DaletVideoIndexerVideos', function($scope) {
+        $scope.current = {};
         $scope.videos = [];
         $scope.BASE_URL = "http://dev-moshe:3000/api";
         $scope.loadAllVideoIds = function(callback) {
@@ -26,16 +27,21 @@ angular.module('daletVideoIndexer', [])
             });
         };
 
+        $scope.onVideoClick = function(video) {
+            console.log(video.id);
+            $scope.current = video;
+        };
+
         $scope.loadAllVideoIds(function (tmpVideos) {
             JSON.parse(tmpVideos).forEach(function (t) {
                 $scope.loadVideoData(t, function (tmpVideo) {
                     var obj = JSON.parse(tmpVideo);
-                    if (obj.thumbnailUrl != undefined) {
-                        console.log(obj.thumbnailUrl);
-                        $scope.$apply(function () {
-                            $scope.videos.push(obj);
-                        });
-                    };
+                    if ($scope.videos.length == 0) {
+                        $scope.onVideoClick(obj);
+                    }
+                    $scope.$apply(function () {
+                        $scope.videos.push(obj);
+                    });
                 });
             });
         });
