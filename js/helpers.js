eae49abfcce09907706cf72d9204cd92c8792249
Sergey Yakovlev <lokkersp@gmail.com>
2017-10-31 18:45:47 +0200

    add base support of JSON traversal

---
+++ js/helpers.js
@@ -0,0 +1,58 @@
+function map(collection,func) {
+	var rc = []
+	collection.forEach(function(i){rc.push(f(i));});
+	return rc;
+}
+function filter(collection,predicate){
+	var rc = []
+	collection.forEach(function(i){
+		if(predicate(i) {
+			rc.push(i);
+		}
+	});
+	return rc;
+}
+function getAudioEffectCategories(doc){
+	var rs = [];
+	doc.insights.audioEffectsCategories.forEach(c=>rs.push({"key":c.key,"type":c.type}));
+	return rs;
+}
+
+function getAllParticipants(doc) {
+	var rs = [];
+	doc.insights.participants.forEach(c=>rs.push({"id":c.id,"name":c.name,"pictureUrl":c.pictureUrl}));
+	return rs;
+}
+
+function getFaces(doc) {
+	var rs = [];
+	doc.insights.faces.forEach(f=>rs.push({
+		"id":f.id,
+		"bingId":f.bingId,
+		"confidence":f.confidence,
+		"description",f.description,
+		"imageUrl":f.imageUrl,
+		"knownPersonId":f.knownPersonId,
+		"name":f.name,
+		"thumbnailId":f.thumbnailId,
+		"title":f.title
+	}));
+	return rs;
+}
+
+function topics(doc) {
+	var rs = [];
+	doc.insights.topics.forEach(t=>rs.push({
+		"id"   : t.id,
+		"name" : t.name,
+		"stem" : t.stem,
+		"rank" : t.rank,
+		"words": t.words
+	}));
+	return rs;
+}
+
+function getWordsFromTopic(topic) {
+	var obj = [];
+	return topic.words[0].split(" ");
+}
