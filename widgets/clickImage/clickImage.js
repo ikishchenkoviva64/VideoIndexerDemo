da2986e560e9a518d8da1d42466049ce47afa4d2
arkadash <arkadash@post.bgu.ac.il>
2017-10-31 08:55:27 +0200

    Rest request

--- widgets/clickImage/clickImage.js
+++ widgets/clickImage/clickImage.js
@@ -6,24 +6,12 @@ $.widget("ui.clickImage", {
 
     // Options
     options: {
-        src: "https://pbs.twimg.com/profile_images/416826618145820673/uRRl8e_9_400x400.png",
-        title: null,
-        onClick: function(){
-            alert('Clicked!');
-        }
+
     },
 
     // Constructor
     _create: function(){
-        const that = this;
-        const el = $(this.element).addClass("click-image");
-        el.append(
-            $('<img class="click-image-content" src= ' + that.options.src + ' alt="Avatar">')
-        );
-        if(that.options.title) {
-            el.attr("title", that.options.title);
-        }
-        el.click(that.options.onClick);
+
     }
 
 
